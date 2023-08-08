c639d9320cc6a9616a28febe05f0f7b6b97d9d6b
GeorgieEggleton <georgie@sailingfaster.co.uk>
2023-08-08 17:10:39 +0000

    add code for form to add new task

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidebar initialization 
     let sidenav  = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // date Picker
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmm, yyyy",
+      i18n: {done: "Select"}
+    });
+
+    // dropdown selection initlazation 
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+
   });
+
